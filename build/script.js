0b6601c6f6f4c4d6a6f1b2eaf911aa7e645230f0
Guillermo Sicilia Hernandez <guillermo.sicilia@atos.net>
2023-08-16 14:47:41 +0100

    :construction: About page enhancement

--- build/script.js
+++ build/script.js
@@ -1,5 +1,20 @@
-let menu = document.getElementById("menu");
+let sections = document.getElementsByClassName("about-sect");
 
-function toggleNavbar() {
-    menu.classList.toggle("active");
+for (let i = 1; i < sections.length; i++) {
+    sections[i].classList.add("hidden")
+}
+
+function toggleActiveAbout(option) {
+    let options = document.getElementsByClassName("about-opt");
+
+    for (const opt of options) {
+        opt.classList.remove("active");
+    }
+
+    for (const section of sections) {
+        section.classList.add("hidden")
+    }
+
+    document.getElementById(option.id.substring(1)).classList.remove("hidden");
+    option.classList.add("active");
 }
