9f4937269d092b6c19ba40efb0b0d3f6193cd8af
Guillermo Sicilia Hernandez <guillermo.sicilia@eviden.com>
2024-02-15 15:35:05 +0000

    :white_check_mark: Creadas cartas tools

--- build/script.js
+++ build/script.js
@@ -1,4 +1,11 @@
 let sections = document.getElementsByClassName("about-sect");
+let origTitle = document.title;
+window.onblur = () => {
+    document.title = "No te vayas..."
+};
+window.onfocus = () => {
+    document.title = origTitle;
+};
 
 for (let i = 1; i < sections.length; i++) {
     sections[i].classList.add("hiddenplus")
@@ -7,7 +14,6 @@ for (let i = 1; i < sections.length; i++) {
 function changeInfoAbout(option) {
     let options = document.getElementsByClassName("about-opt");
     let section = document.getElementById(option.id.substring(1));
-    console.log(section)
 
     for (const opt of options) {
         opt.classList.remove("active");
@@ -19,6 +25,25 @@ function changeInfoAbout(option) {
 
     section.classList.add("slide");
     option.classList.add("active");
+
+    setTimeout(() => { section.classList.remove("hiddenplus") }, 200);
+    setTimeout(() => { section.classList.remove("slide") }, 400);
+}
+
+function changeInfoAboutMobile(event) {
+    let options = document.getElementsByClassName("about-opt");
+    let section = document.getElementById(event.target.options[event.target.selectedIndex].value);
+
+    for (const opt of options) {
+        opt.classList.remove("active");
+    }
+
+    for (const section of sections) {
+        section.classList.add("hiddenplus")
+    }
+
+    section.classList.add("slide");
+    document.getElementById("a" + event.target.options[event.target.selectedIndex].value).classList.add("active");
 
     setTimeout(() => { section.classList.remove("hiddenplus") }, 200);
     setTimeout(() => { section.classList.remove("slide") }, 400);
